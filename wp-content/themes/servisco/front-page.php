a13bb0f4f2dfe8cfce6c30f229885a6f3aa7b606
Zenike <nicolasmaquet@gmail.com>
2018-11-27 16:32:05 +0100

    fin du banner home + groupe de services

--- wp-content/themes/servisco/front-page.php
+++ wp-content/themes/servisco/front-page.php
@@ -32,112 +32,87 @@
 	</div>
 </div>
 
-<div class="scrollsignal">
-	<span class="scrollsignal__topbar"></span>
-	<button class="scrollsignal__arrow">
-		<i class="fa fa-chevron-down"></i>
-	</button>
-	<span class="scrollsignal__bottombar"></span>
-</div>
+<?php get_template_part('inc/scrollsignal'); ?>
 
-
-
-
-
-
-
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-
-
-
-
-<h1>
+<h2 class="ctitle mgb90 mgt120 autosize-m">
 	<?php the_field('agent_mushthave_title'); ?>
-</h1>
+</h2>
 
+<!-- Zone liste des packs en deux colonnes -->
 
+<section class="packsgroups autosize-m">
+	<?php
+	// crée un tableau avec les deux ACF group pour les manipuler en une seule grande boucle
+	$packs = array('basic_pack','advanced_pack');
+	$option_nbr = 1;
 
+	// compte le nombre de services pour diviser ensuite en colonnes
+	$count_services_cpt = wp_count_posts('services');
+	$published_services_cpt = $count_services_cpt->publish;
+	$services_cpt_per_column = $published_services_cpt / 2;
+	?>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+	<?php foreach ($packs as $pack_name) : ?>
+		<div class="packsgroups__column">
+			<?php if(have_rows($pack_name)): ?>
+			<?php while(have_rows($pack_name)) : the_row(); ?>
 
+				<div class="packsgroups__head">
+					<h3 class="packsgroups__title">
+						<?php the_sub_field('title'); ?>
+					</h3>
 
+					<div class="packsgroups__text">
+						<?php the_sub_field('intro'); ?>
+					</div>
+				</div>
 
+				<?php
+					// parametres dynamiques pour le filtre WP QUERY
+					if($pack_name == "advanced_pack") {
+						$offset = $services_cpt_per_column;
+					} else {
+						$offset = 0;
+					}
 
+					// paramètres du filtre WP QUERY
+					$args = array(
+						'post_type'			=> 'services',
+						'posts_per_page'	=> $services_cpt_per_column,
+						'offset'			=> $offset
+					);
 
+					// WP QUERY
+					$loop = new WP_Query($args);
+				?>
+				<ul class="packsgroups__list">
+					<?php while ($loop->have_posts() ) : $loop->the_post(); ?>
+						<li class="packsgroups__service">
+							<a href="<?php echo get_page_link(11) . '?option=' . $option_nbr; ?>">
+								<?php get_template_part('svg/icon-check'); ?>
+								<?php the_title(); ?>
+							</a>
+						</li>
+						<?php $option_nbr++; ?>
+					<?php endwhile; ?>
+				</ul>
+				<?php wp_reset_postdata(); ?>
 
-<?php
-// crée un tableau avec les deux ACF group pour les manipuler en une seule grande boucle
-$packs = array('basic_pack','advanced_pack');
-$option_nbr = 1;
-
-// compte le nombre de services pour diviser ensuite en colonnes
-$count_services_cpt = wp_count_posts('services');
-$published_services_cpt = $count_services_cpt->publish;
-$services_cpt_per_column = $published_services_cpt / 2;
-?>
-<?php foreach ($packs as $pack_name) : ?>
-	<?php if(have_rows($pack_name)): ?>
-	<?php while(have_rows($pack_name)) : the_row(); ?>
-
-		<h1>
-			<?php the_sub_field('title'); ?>
-		</h1>
-
-		<div>
-			<?php the_sub_field('intro'); ?>
-		</div>
-
-		<?php
-			// parametres dynamiques pour le filtre WP QUERY
-			if($pack_name == "advanced_pack") {
-				$offset = $services_cpt_per_column;
-			} else {
-				$offset = 0;
-			}
-
-			// paramètres du filtre WP QUERY
-			$args = array(
-				'post_type'			=> 'services',
-				'posts_per_page'	=> $services_cpt_per_column,
-				'offset'			=> $offset
-			);
-
-			// WP QUERY
-			$loop = new WP_Query($args);
-		?>
-		<ul>
-			<?php while ($loop->have_posts() ) : $loop->the_post(); ?>
-				<li>
-					<a href="<?php echo get_page_link(11) . '?option=' . $option_nbr; ?>">
-						<?php the_title(); ?>
+				<div class="packsgroups__link">
+					<a class="cbutton" href="<?php echo get_page_link(11) . '&option=' . $pack_name; ?>">
+						<?php the_sub_field('button_text'); ?>
 					</a>
-				</li>
-				<?php $option_nbr++; ?>
+				</div>
+
 			<?php endwhile; ?>
-		</ul>
-		<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+		</div>
+	<?php endforeach; ?>
+</section>
 
-		<a href="<?php echo get_page_link(11) . '&option=' . $pack_name; ?>">
-			<?php the_sub_field('button_text'); ?>
-		</a>
-
-	<?php endwhile; ?>
-	<?php endif; ?>
-<?php endforeach; ?>
+<h2 class="ctitle mg90 autosize-m">
+	<?php the_field('agent_mushthave_title'); ?>
+</h2>
 
 <br>
 <br>
---
+++ wp-content/themes/servisco/inc/scrollsignal.php
@@ -0,0 +1,9 @@
+<div class="scrollsignal">
+	<span class="scrollsignal__topbar"></span>
+
+	<button class="scrollsignal__arrow">
+		<i class="fa fa-chevron-down"></i>
+	</button>
+
+	<span class="scrollsignal__bottombar"></span>
+</div>
