02e11f157864f088cb5f787ba8bc2fb70f9e052e
web101 <web101@flexvision.be>
2019-07-02 11:08:14 +0200

    update core 5.2.2.

---
+++ wp-content/wp-rocket-config/servisco.immo.php
@@ -0,0 +1,16 @@
+<?php
+defined( 'ABSPATH' ) || die( 'Cheatin&#8217; uh?' );
+
+$rocket_cookie_hash = 'dad58bd56513a8dae7a4c02c5fff4b94';
+$rocket_logged_in_cookie = 'wordpress_logged_in_dad58bd56513a8dae7a4c02c5fff4b94';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_cache_mobile = 1;
+$rocket_cache_ssl = 1;
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(?:.+/)?embed/|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_.+|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array();
+$rocket_secret_cache_key = '5cd091c77d728929205815';
+$rocket_do_caching_mobile_files = 0;
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array();
