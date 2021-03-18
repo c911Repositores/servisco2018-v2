0550259df6bf9bb322ad5cf605d427c852c1ac66
Cesar Silva <cesar@cactus911.com>
2021-03-18 11:41:08 -0600

    Modifications servisco

--- wp-content/themes/servisco/functions.php
+++ wp-content/themes/servisco/functions.php
@@ -4,9 +4,9 @@
 ///// Import des fichiers css et js du theme
 //////////////////////////////////////////////////////////////////////////////////////
 function theme_scripts_and_css() {
-	wp_enqueue_script('scriptfile', get_stylesheet_directory_uri() . '/assets/js/js-dist.js', array('jquery'));
+	wp_enqueue_script('scriptfile', get_stylesheet_directory_uri() . '/assets/js/js-dist.js', array('jquery'), time());
 	// wp_enqueue_script('jqueryuifile', get_stylesheet_directory_uri() . '/node_modules/jquery-ui/js-dist.js', array('jquery'));
-	wp_enqueue_style('stylefile', get_stylesheet_directory_uri() . '/assets/css/style.css');
+	wp_enqueue_style('stylefile', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), time());
 }
 add_action('wp_enqueue_scripts', 'theme_scripts_and_css');
 
